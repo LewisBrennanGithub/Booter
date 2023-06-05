04f9e1725d4defde2affb4a62251dd2fc3f03cbc
Lewis Brennan <lewisbrennan444@gmail.com>
2023-06-05 11:28:17 +0100

    initial commit

---
+++ booter_java/src/main/java/com/booter/booter/BooterApplication.java
@@ -0,0 +1,13 @@
+package com.booter.booter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooterApplication.class, args);
+	}
+
+}
---
+++ booter_java/src/test/java/com/booter/booter/BooterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booter.booter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
