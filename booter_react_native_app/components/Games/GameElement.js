02f010f22904c6815064641e4c1dc5053960c39a
Lewis Brennan <lewisbrennan444@gmail.com>
2023-06-14 10:15:37 +0100

    game players are displayed but not game creators

--- booter_react_native_app/components/Games/GameElement.js
+++ booter_react_native_app/components/Games/GameElement.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+// REMOVE USEEFFECT
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import GameUpdateForm from './GameUpdateForm';
+import * as GameServices from "../../services/GameServices";
 
 const GameElement = ({ players, game, handleDeleteGame, handleJoinGame, loggedPlayer }) => {
   const [isEditing, setIsEditing] = useState(false);
 
+  // DELETE BELOW THIS LINE
+  // TESTING PURPOSES ONLY - DELETE THIS
+  const [gamePlayers, setGamePlayers] = useState([]);
+  // TESTING PURPOSES ONLY - DELETE THIS
+  useEffect(() => {
+    fetchGamePlayers();
+  }, [game]);
+  // TESTING PURPOSES ONLY - DELETE THIS
+  const fetchGamePlayers = async () => {
+    try {
+      const fetchedGamePlayers = await GameServices.getGamePlayers(game.id);
+      setGamePlayers(fetchedGamePlayers);
+    } catch (err) {
+      console.error('Error fetching game players:', err);
+    }
+  };
+  // DELETE ABOVE THIS LINE
+  
   const playerIsInGame = game?.players?.some(player => player.id === loggedPlayer?.id);
   const gameIsFull = (game.players?.length ?? 0) >= game.maxPlayers;
 
@@ -48,6 +68,9 @@ const GameElement = ({ players, game, handleDeleteGame, handleJoinGame, loggedPl
           <Text>Actual Seriousness Level: {game.actualSeriousnessLevel}</Text>
           <Text>Completed: {game.completedStatus ? 'Yes' : 'No'}</Text>
           <Text>Max Players: {game.maxPlayers}</Text>
+          {gamePlayers.map(player => (
+  <Text key={player.id}>Player: {player.userName}</Text>
+))}
           {!playerIsInGame && !gameIsFull && (
             <Button title="Join" onPress={() => handleJoinGame(game.id, loggedPlayer)} />
           )}
