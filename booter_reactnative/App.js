6e4be29583fe7f750efe0ee96d907082aad4cf78
Lewis Brennan <lewisbrennan@Lewiss-MacBook-Air.local>
2023-08-06 13:15:00 +0100

    fixed minor complaint from auth0 regarding children components in appjs

--- booter_reactnative/App.js
+++ booter_reactnative/App.js
@@ -15,10 +15,12 @@ const MyTheme = {
 export default function App() {
   return (
     <Auth0Provider domain={"dev-jbsvjbwih2gygs04.uk.auth0.com"} clientId={"rPScIrLIXAkRbZtUgbdvDsarHIetMx2g"}>
+      <>
     <StatusBar barStyle="light-content" /> 
     <NavigationContainer theme={MyTheme}>
         <BooterContainer />
     </NavigationContainer>
+    </>
     </Auth0Provider>
   );
 } 
